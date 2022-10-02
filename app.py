50ae91b7db39148d8bf8a3c039db21f6910838d3
Anushkaaa08 <102422347+Anushkaaa08@users.noreply.github.com>
2022-10-02 16:01:32 +0530

    final changes for deployment

--- app.py
+++ app.py
@@ -2,12 +2,19 @@ import numpy as np
 from flask import Flask, render_template, request
 import model
 
-app = FLask (__name__)
+app = Flask (__name__)
 
 
-@app.route('/')
+@app.route("/", methods = ['POST'])
 def home():
     return model
+
+@app.route('/')
+def intro():
+    if request.method == "POST":
+        name = request.form['fname']
+        mode_pred = model.predict()
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
